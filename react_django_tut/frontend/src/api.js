0779ec66f626c9601a7be8db531eb5cc6a9d2289
James Steward <mrstew1997@gmail.com>
2025-08-17 18:19:38 +0100

    Fixing axios network error

--- react_django_tut/frontend/src/api.js
+++ react_django_tut/frontend/src/api.js
@@ -4,7 +4,8 @@ import { ACCESS_TOKEN } from "./constants"
 const apiURL = "/choreo-apis/backend-389948499:8000"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
+    // baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
+    baseURL: apiURL,
 })
 
 api.interceptors.request.use(
